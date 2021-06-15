7aacc253a2242e717acb5a4f5b2d12388bc67a16
lastpingu <54551308+lazypwny751@users.noreply.github.com>
2021-06-15 14:08:24 +0300

    kraken metadata

--- index.sh
+++ index.sh
@@ -1,1 +1,8 @@
-# aA
+maintainer="ByCh4n"
+reponame="kraken-repo"
+
+packages=(
+  "https://raw.githubusercontent.com/ByCh4n-Group/kraken-repo/main"
+  "bchacktool3.tar.gz_bchacktool_3.0.0"
+  "alvatadro.tar.gz_rzcracker_1.0.0"
+)
